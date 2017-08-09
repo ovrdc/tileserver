8a958ab819896d4382b2a63cffb6eb2b59e64a19
OVRDC GIS Dev Team <gis.ovrdc@gmail.com>
2017-08-09 12:16:42 -0400

    Update tileserver.js

--- tileserver.js
+++ tileserver.js
@@ -5,8 +5,8 @@ var express = require("express"),
   fs = require('fs'),
   path = require('path'),
   diskspace = require('diskspace');
-  require('prototypes');
-var pusage = require('pidusage')
+require('prototypes');
+var pusage = require('pidusage');
 
 app.use('/preview', express.static(__dirname + '/preview'));
 
@@ -46,8 +46,9 @@ getUsage();*/
   console.log(uriList)
 });*/
 
-//create a tiles object that has all mbtiles in it and a metadata objec that has all tile metadata in it
+/*create a tiles object that has all mbtiles in it and a metadata objec that has all tile metadata in it*/
 var tiles, metadata;
+
 function getTileData(e, callback) {
 
   tiles = [];
@@ -86,68 +87,73 @@ function getTileData(e, callback) {
     });
   });
 }
+
 getTileData();
 
-// Set return header
+/* Set return header*/
 function getContentType(t) {
   var header = {};
 
-  // CORS
+  /* CORS*/
   header["Access-Control-Allow-Origin"] = "*";
   header["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept";
 
-  // Cache
-  //header["Cache-Control"] = "public, max-age=2592000";
+  /* Cache*/
 
-  // request specific headers
+  if (t === "json") {
+    header["Cache-Control"] = "no-cache, no-store, must-revalidate";
+  }
+
+  /* request specific headers*/
   if (t === "png") {
     header["Content-Type"] = "image/png";
+    header["Cache-Control"] = "public, max-age=604800";
   }
   if (t === "jpg") {
     header["Content-Type"] = "image/jpeg";
+    header["Cache-Control"] = "public, max-age=604800";
   }
   if (t === "pbf") {
     header["Content-Type"] = "application/x-protobuf";
     header["Content-Encoding"] = "gzip";
+    header["Cache-Control"] = "public, max-age=604800";
   }
 
   return header;
 }
 
-app.get('/', function(req, res) {
-  if (req.url === '/' || req.url === "/index.html") {
-    //console.log('html2');
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
+/* tile cannon adapted from mbtiles-server */
+app.get('/:s/:z/:x/:y.:t', function(req, res) {
+  /*console.log(req.params);*/
+  var fileCheck = tilesDir + req.params.s + '.mbtiles';
+  if (fs.existsSync(fileCheck)) {
+    /*console.log(fileCheck);*/
+    new MBTiles(p.join(tilesDir, req.params.s + '.mbtiles'), function(err, mbtiles) {
+      //console.log(req.params);
+      mbtiles.getTile(req.params.z, req.params.x, req.params.y, function(err, tile, headers) {
+        /*added for when there are no tiles at this location but the mbtiles file does exist*/
+        if (err) {
+          res.set({
+            "Access-Control-Allow-Origin": "*"
+          });
+          res.set({
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+          });
+          res.set({
+            "Content-Type": "text/plain"
+          });
+          res.status(204).send('Tile rendering error: ' + err + '\n');
+        } else {
+          res.set(getContentType(req.params.t));
+          res.send(tile);
+        }
+      });
+      if (err) console.log("error opening database");
     });
-    fs.createReadStream('index.html').pipe(res);
   }
 });
 
-// tile cannon
-app.get('/:s/:z/:x/:y.:t', function(req, res) {
-  new MBTiles(p.join(tilesDir, req.params.s + '.mbtiles'), function(err, mbtiles) {
-    //console.log(req.params);
-    mbtiles.getTile(req.params.z, req.params.x, req.params.y, function(err, tile, headers) {
-      if (err) {
-        res.set({
-          "Access-Control-Allow-Origin": "*"
-        });
-        res.set({
-          "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-        });
-        res.set({
-          "Content-Type": "text/plain"
-        });
-        res.status(204).send('Tile rendering error: ' + err + '\n');
-      } else {
-        res.set(getContentType(req.params.t));
-        res.send(tile);
-      }
-    });
-    if (err) console.log("error opening database");
-  });
-});
+/*set request options for index, json and loader files*/
 
 //Need help here with getting any json I create in this app to send - just have individual ones right now
 /*app.get("/:d.:f", function(req, res) {
@@ -158,38 +164,55 @@ app.get('/:s/:z/:x/:y.:t', function(req, res) {
   }
 });*/
 
-/*States for index page*/
+app.get('/', function(req, res) {
+  if (req.url == '/' || req.url == "/index.html") {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    fs.createReadStream('index.html').pipe(res);
+  }
+});
 
-var mem = 0;
-var space = [];
-var cpu = 0;
+app.get('/loaderio-e9020011b3d721afa9b9a497fc6d24a3.html', function(req, res) {
+  console.log('loader');
+  fs.createReadStream('loaderio-e9020011b3d721afa9b9a497fc6d24a3.html').pipe(res);
+});
 
-diskspace.check('/', function(err, total, free, status) {
-  var f = (free / 1073741824).toFixed(2);
-  var t = (total / 1073741824).toFixed(2);
-  space.push({
-    freespace: f,
-    totalspace: t,
-    memory: mem,
-    cpu: cpu
+app.get('/index.html', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
   });
-  console.log(free, total);
+  fs.createReadStream('index.html').pipe(res);
 });
 
 app.get('/space.json', function(req, res) {
 
-  //console.log('getting json file');
-  //memory usage
+  var mem = 0;
+  var space = [];
+  var cpu = 0;
+
+  diskspace.check('/', function(err, total, free, status) {
+    var f = (free / 1073741824).toFixed(2);
+    var t = (total / 1073741824).toFixed(2);
+    space.push({
+      freespace: f,
+      totalspace: t,
+      memory: mem,
+      cpu: cpu
+    });
+    console.log(free, total);
+  });
+
   pusage.stat(process.pid, function(err, stat) {
     mem = stat.memory / 1048576;
     space[0].memory = mem;
     space[0].cpu = stat.cpu;
     console.log('Pcpu: %s', stat.cpu)
-    console.log('Mem: %s', stat.memory / 1048576) //those are bytes
+    console.log('Mem: %s', stat.memory / 1048576) /*those are bytes*/
     pushRequest()
   })
 
-  // Unmonitor process
+  /* Unmonitor process*/
   pusage.unmonitor(process.pid);
 
   function pushRequest() {
@@ -199,6 +222,9 @@ app.get('/space.json', function(req, res) {
 });
 
 app.get('/tiles.json', function(req, res) {
+
+  getTileData();
+
   //console.log('getting json file');
   res.set(getContentType("json"));
   res.send(tiles);
@@ -214,7 +240,7 @@ app.get('/metadata.json', function(req, res) {
   }
 });
 
-/*end stats*/
+/*end stats and static files*/
 
 /*start up the server*/
 console.log('Listening on port: ' + 3000);
