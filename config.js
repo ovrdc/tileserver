e91f702a0683e50f26ce8229ab688a93892bf75a
malcolm <malcolmtmeyer@gmail.com>
2017-09-29 11:42:03 -0400

    updates

---
+++ config.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var path = require('path');
+
+var config = {
+  PORT: 3000,
+  TILES_DIR: path.join(__dirname, '/tiles/'),
+  PREVIEW_DIR: path.join(__dirname, '/preview/'),
+  DATA_DIR: path.join(__dirname, '/data/')
+}
+
+module.exports = config;
+
+
+/*"use strict";
+
+var path = require('path');
+
+var config = {
+  PORT: 3000,
+  IPADDRESS: '127.0.0.1',
+  TILES_DIR: path.join(__dirname, '/data/'),
+  PREVIEW_DIR: path.join(__dirname + '/preview/'),
+  SUBDMAINS: true,
+  HOSTNAME: 'tiles.ovrdc.org'
+}
+module.exports = config;*/
