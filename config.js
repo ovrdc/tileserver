df1be213c40fe32d72eae1020432d8b99212e539
reyemtm <malcolmtmeyer@gmail.com>
2017-10-24 09:47:54 -0400

    working local windows

--- config.js
+++ config.js
@@ -3,20 +3,22 @@
 var path = require('path');
 
 var config = {
-  SYSTEM: 'nix',
-  PORT: 3000,
+  SYSTEM: 'win',
+  PORT: 80,
   TILES_DIR: path.join(__dirname, '/tiles/'),
   PREVIEW_DIR: path.join(__dirname, '/preview/'),
   DATA_DIR: path.join(__dirname, '/data/'),
   STATIC_DIR: path.join(__dirname, '/static/'),
-  URL: "tileserver.ovrdc.org/",
-  URL_PREFIX: "https://",
+  //url with trailing slash
+  URL: "127.0.0.1/",
+  //http or https
+  URL_PREFIX: "http://",
   META_DIR: path.join(__dirname, '/meta/'),
   /*
   * For subdmains, use a blank "" as the first subdmain to serve tiles from the url as well
-  *
+  *   SUBDOMAINS: ['', 'a', 'b', 'c'],
   */
-  SUBDOMAINS: ['', 'a', 'b', 'c'],
+  SUBDOMAINS: [''],
   MONIT: true
 }
 
